76b9fe4f666376f2efa06b08f1bb856fe6dc8daf
brad <brad@blackbird>
2012-11-11 18:03:15 -0800

    Correct typos

--- squareplay/src/calls/centers.py
+++ squareplay/src/calls/centers.py
@@ -1,3 +1,21 @@
+'''
+    Copyright 2012 Brad Christie
+
+    This file is part of TAMinations.
+
+    Taminations is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Taminations is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Taminations.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 class Centers(Call):
 
@@ -12,5 +30,5 @@ class Centers(Call):
       raise NoDancerError()
     ctx.active = newactive;
 
-caller['classes']['cemters'] = Centers
-caller['classes']['cemter'] = Centers
+caller['classes']['centers'] = Centers
+caller['classes']['center'] = Centers
--- squareplay/src/calls/verycenters.py
+++ squareplay/src/calls/verycenters.py
@@ -1,3 +1,21 @@
+'''
+    Copyright 2012 Brad Christie
+
+    This file is part of TAMinations.
+
+    Taminations is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Taminations is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Taminations.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 class VeryCenters(Call):
 
@@ -12,4 +30,4 @@ class VeryCenters(Call):
       raise NoDancerError()
     ctx.active = newactive;
 
-caller['classes']['verycemters'] = VeryCenters
+caller['classes']['verycenters'] = VeryCenters
