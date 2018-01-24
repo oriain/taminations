e178e20b58e4f8025e2e962c9e86415f9aea925f
Brad Christie <brad@bradchristie.com>
2018-01-24 13:47:30 -0800

    Clean up

--- src/calls/run.js
+++ src/calls/run.js
@@ -36,12 +36,12 @@ define(['calls/action','path','callerror'], (Action,Path,CallError) =>
         if (d.active) {
           //  Find dancer to run around
           //  Usually it's the partner
-          var d2 = d.partner
+          let d2 = d.partner
           if (!d2)
             throw new CallError(`Dancer ${d} has nobody to Run around.`)
           //  But special case of t-bones, could be the dancer on the other side,
           //  check if another dancer is running around this dancer's "partner"
-          var d3 = d2.partner
+          let d3 = d2.partner
           if (d != d3 && d3 && d3.active) {
             d2 = ctx.dancerToRight(d)
             if (d2 == d.partner)
@@ -50,21 +50,21 @@ define(['calls/action','path','callerror'], (Action,Path,CallError) =>
           //  Partner must be inactive
           if (!d2 || d2.active)
             throw new CallError(`Dancer ${d} has nobody to Run around.`)
-          var m = ctx.isRight(d,d2) ? 'Run Right' : 'Run Left'
-          var dist = ctx.distance(d,d2)
+          let m = ctx.isRight(d,d2) ? 'Run Right' : 'Run Left'
+          let dist = ctx.distance(d,d2)
           d.path = TamUtils.getMove(m).scale(1,dist/2)
           //  Also set path for partner
-          if (ctx.isRight(d2,d))
-            m = "Dodge Right"
-          else if (ctx.isLeft(d2,d))
-            m = "Dodge Left"
-          else if (ctx.isInFront(d2,d))
-            m = "Forward 2"
-          else if (ctx.isInBack(d2,d))
-            m = "Back 2"   //  really ???
-          else
-            m = "Stand"   // should never happen
-          d2.path = TamUtils.getMove(m).scale(1,dist/2)
+          let m2 = If (ctx.isRight(d2,d))
+            .Then ("Dodge Right")
+          .ElseIf (ctx.isLeft(d2,d))
+            .Then ("Dodge Left")
+          .ElseIf (ctx.isInFront(d2,d))
+            .Then("Forward 2")
+          .ElseIf (ctx.isInBack(d2,d))
+            .Then("Back 2")   //  really ???
+          .Else
+            ("Stand")   // should never happen
+          d2.path = TamUtils.getMove(m2).scale(1,dist/2)
         }
       })
 
